277017eefdd36b4e561656df3b4a089c0067b5d1
Chanupa deshan <chanupadeshan2002@gmail.com>
2024-07-06 16:03:48 +0530

    got final page number

--- GPUprice.py
+++ GPUprice.py
@@ -9,4 +9,10 @@ page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
 
 page_text = doc.find(class_="list-tool-pagination-text")
-print(page_text)
+final_page = str(page_text).split("/")[1].split(">")[1].split("<")[0]
+
+
+for page in range(1,final_page+1):
+    url = f"https://www.newegg.com/p/pl?d={input}&page={page}"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
