3f2ff02d4f3f43072b5380e36f7feab9494b2b21
Chanupa deshan <chanupadeshan2002@gmail.com>
2024-07-07 19:57:53 +0530

    user can Input file name also

--- GPUprice.py
+++ GPUprice.py
@@ -3,46 +3,45 @@ import requests
 import re
 import csv
 from openpyxl import Workbook
-import ui 
 
-search = ui.user_input
 
-url = f"https://www.newegg.com/p/pl?d={search}"
-page = requests.get(url).text
-doc = BeautifulSoup(page, "html.parser")
+def scrap_gpu_price(search,file_name):
+            url = f"https://www.newegg.com/p/pl?d={search}"
+            page = requests.get(url).text
+            doc = BeautifulSoup(page, "html.parser")
 
-page_text = doc.find(class_="list-tool-pagination-text")
-final_page = str(page_text).split("/")[1].split(">")[1].split("<")[0]
-print(f"Total Number of pages:{final_page}")
+            page_text = doc.find(class_="list-tool-pagination-text")
+            final_page = str(page_text).split("/")[1].split(">")[1].split("<")[0]
+            print(f"Total Number of pages:{final_page}")
 
-wb = Workbook()
-ws = wb.active
-ws.append([f"Number of pages: {final_page}"])
-ws.append(["Item", "Price"])
+            wb = Workbook()
+            ws = wb.active
+            ws.append([f"Number of pages: {final_page}"])
+            ws.append(["Item", "Price"])
 
-for page_num in range(1, int(final_page) + 1):
-    page_url = f"https://www.newegg.com/p/pl?d={search}&page={page_num}"
-    page_content = requests.get(page_url).text
-    page_doc = BeautifulSoup(page_content, "html.parser")
+            for page_num in range(1, int(final_page) + 1):
+                page_url = f"https://www.newegg.com/p/pl?d={search}&page={page_num}"
+                page_content = requests.get(page_url).text
+                page_doc = BeautifulSoup(page_content, "html.parser")
 
-    # Correct class name for item listing
-    div = page_doc.find(class_="item-cells-wrap border-cells short-video-box items-grid-view four-cells expulsion-one-cell")
-    if div:
-        items = div.find_all(text=re.compile(search, re.IGNORECASE))
-        for item in items:
-            print(item)
+                # Correct class name for item listing
+                div = page_doc.find(class_="item-cells-wrap border-cells short-video-box items-grid-view four-cells expulsion-one-cell")
+                if div:
+                    items = div.find_all(text=re.compile(search, re.IGNORECASE))
+                    for item in items:
+                        print(item)
 
-            parent_price = item.find_parent(class_="item-container")
-            price = parent_price.find("li", class_="price-current")
-            price_split_sign = str(price).split("/")[1].split(">")[1].split("<")[0]
-            price_split_amount = str(price).split("/")[1].split(">")[2].split("<")[0]
-            Total_price = f"{price_split_sign} {price_split_amount}"
-            print(price_split_sign, price_split_amount)
-            print("\n")
+                        parent_price = item.find_parent(class_="item-container")
+                        price = parent_price.find("li", class_="price-current")
+                        price_split_sign = str(price).split("/")[1].split(">")[1].split("<")[0]
+                        price_split_amount = str(price).split("/")[1].split(">")[2].split("<")[0]
+                        Total_price = f"{price_split_sign} {price_split_amount}"
+                        print(price_split_sign, price_split_amount)
+                        print("\n")
 
-            ws.append([item, Total_price])
+                        ws.append([item, Total_price])
 
-    else:
-        print(f"No items found on page {page_num}")
+                else:
+                    print(f"No items found on page {page_num}")
 
-wb.save("GPUprice.xlsx")
+            wb.save(file_name)
--- ui.py
+++ ui.py
@@ -1,6 +1,23 @@
 import streamlit as st
+import GPUprice as gpu
+import re
 
-st.title('GPU price Scraper')
+st.title('GPU price Scraper 📜')
 st.image("C:\projects\Web scraping\GPU-prices\gpu image.jpg")
 user_input = st.text_input("What GPU are you Looking for?")
-st.button("Search")
+file_name = st.text_input("Enter the file name to save the results(only xlsx files are supported)")
+
+pattern = f"\."
+if re.search(pattern,file_name):
+    split_name = file_name.split(".")[0]
+
+else:
+    split_name = file_name   
+
+file_name = f"{split_name}.xlsx"
+
+if st.button("Search"):
+    st.write(f"Searching 🔎")
+    gpu.scrap_gpu_price(user_input,file_name)
+    st.write(f"Scraping Complete. Check the file {file_name} for the results.💯")
+
